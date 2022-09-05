077aef243f107617e8147790872cfffe29afd173
agoryelov <andrey.goryelov@gmail.com>
2022-09-05 11:35:23 -0700

    Add optional custom bot prefix

--- PerudoBot.Discord/main.py
+++ PerudoBot.Discord/main.py
@@ -9,35 +9,10 @@ load_dotenv()
 DISCORD_TOKEN = getenv('DISCORD_TOKEN')
 OWNER_ID = getenv('OWNER_ID')
 MY_GUILD = discord.Object(id=getenv('GUILD_ID'))
+BOT_PREFIX = getenv('BOT_PREFIX', '!')
 
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+bot = commands.Bot(command_prefix=BOT_PREFIX, intents=discord.Intents.all())
 bot.owner_id = int(OWNER_ID)
-
-@bot.command(name='sync_guild', help='To sync application commands for current guild.', hidden=True)
-# Command to sync application commands for current guild.
-@commands.has_permissions(administrator=True)
-async def sync_guild(ctx):
-    if ctx.author.id != bot.owner_id:
-        await ctx.send('You are not my owner!')  # Raise PermissionError
-        return
-    op = await ctx.send('Reloading..')
-    for i in bot.extns:
-        await bot.reload_extension(i)
-    await op.edit(content='Reloaded!')
-    await op.edit(content='Syncing...')
-    await bot.tree.sync(guild=ctx.guild)
-    await op.edit(content='Reloaded and Synced!')
-
-@bot.command(name='reload', help='To reload all cogs.', hidden=True)  # Command to reload all cogs
-@commands.has_permissions(administrator=True)
-async def reload(ctx):
-    if ctx.author.id != bot.owner_id:
-        await ctx.send('You are not my owner!')  # Raise PermissionError
-        return
-    for i in bot.extns:
-        await bot.reload_extension(i)
-    bot.help_command.cog = bot.cogs.get('General')
-    await ctx.send('Reloaded!')
 
 @bot.event
 async def on_ready():
